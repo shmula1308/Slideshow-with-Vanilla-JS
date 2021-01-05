607ac10f5f5cd73c623b1c60044858c9ee02377d
Shpend Mula <shmula@hotmail.com>
2021-01-05 16:39:31 +0100

    changed the approach

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', init);
 let items = [];
 
 function init() {
-    let contentDiv = document.createElement('div');
-    contentDiv.classList.add('slideshow__content');
+    let divC = document.createElement('div');
+    divC.classList.add('slideshow__slide');
     let slideShow = document.querySelector('.slideshow');
-    slideShow.appendChild(contentDiv);
-    createSlideShowBtns();
+    slideShow.prepend(divC);
+
 
     let url = '../slideShow.json';
     fetch(url)
@@ -17,53 +17,35 @@ function init() {
         .catch((err) => {
             console.log('ERROR:', err);
         });
-
 }
 
-function createSlideShowBtns() {
-    let prevBtn = document.createElement('button');
-    prevBtn.classList.add('slideshow__btn', 'slideshow__btn--left')
-    let nextBtn = document.createElement('button');
-    nextBtn.classList.add('slideshow__btn', 'slideshow__btn--right');
-    let playPauseBtn = document.createElement('button');
-    playPauseBtn.classList.add('slideshow__playPause');
-    let contentDiv = document.querySelector('.slideshow__content');
-    contentDiv.append(prevBtn, nextBtn, playPauseBtn)
-}
 
 function loadContent(data) {
-    let dfContent = new DocumentFragment();
+    let dfImages = new DocumentFragment();
     let dfIndicators = new DocumentFragment();
     data.items.forEach((item, idx) => {
-        let div = createItem(item, idx);
+        let img = prepareImage(item, idx);
         let indicator = createIndicators(idx);
-        dfContent.appendChild(div);
+        dfImages.appendChild(img);
         dfIndicators.appendChild(indicator);
     })
-    let contentDiv = document.querySelector('.slideshow__content');
-    contentDiv.prepend(dfContent);
+    let contentDiv = document.querySelector('.slideshow__slide');
+    contentDiv.appendChild(dfImages);
     let slideShowNav = document.querySelector('.slideshow__nav');
     slideShowNav.appendChild(dfIndicators);
-    document.querySelector('.slideshow__item').classList.add('current');
+    document.querySelector('.slideshow__item-image').classList.add('current');
     document.querySelector('.slideshow__indicator').classList.add('current-indicator');
+    items = document.querySelectorAll('.slideshow__item');
+    // start();
 
 }
 
-function createItem(item, index) {
-    let itemDiv = document.createElement('div');
-    itemDiv.classList.add('slideshow__item');
-    itemDiv.setAttribute('data-index', index);
-    let title = document.createElement('h1');
-    title.classList.add('slideshow__item-title');
-    title.textContent = item.title;
-    let p = document.createElement('p');
-    p.classList.add('slideshow__item-text');
-    p.textContent = item.msg;
+function prepareImage(item, index) {
     let img = document.createElement('img');
     img.classList.add('slideshow__item-image');
+    img.setAttribute("data-index", index)
     img.src = `../images/${item.img}`;
-    itemDiv.append(title, p, img);
-    return itemDiv;
+    return img;
 }
 
 function createIndicators(index) {
@@ -72,3 +54,21 @@ function createIndicators(index) {
     button.setAttribute('data-index', index)
     return button;
 }
+
+// function start() {
+//     setInterval(() => {
+//         let [first, ...rest] = items;
+//         items = [...rest, first];
+//         switchItem(0)
+//     }, 3000)
+// }
+
+// function switchItem(index) {
+//     let current = document.querySelector('.current');
+//     current.classList.remove('current');
+//     current.classList.add('leaving');
+//     setInterval(() => {
+//         current.classList.remove('leaving');
+//     }, 800)
+//     items[index].classList.add('current');
+// }
